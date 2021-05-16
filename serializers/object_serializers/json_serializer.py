8ff42a9a84d40231f59f483d3fe21bba6e920e1c
ALYONA29 <alyonazapetskaya@mail.ru>
2021-05-16 23:04:54 +0300

    Add serializers

---
+++ serializers/object_serializers/json_serializer.py
@@ -0,0 +1,19 @@
+#import json
+import serializers.parsers.parser as json_parser
+from serializers.object_serializers.serializer import *
+
+
+class JsonSerializer(ISerializer):
+
+    def dumps(self, obj: object) -> str:
+        return json_parser.dumps(object_to_dict(obj), indent=4)
+
+    def loads(self, s: str) -> object:
+        return json_parser.loads(s)
+
+
+class JsonSerializerCreator(SerializerCreator):
+
+    def create_serializer(self) -> ISerializer:
+        serializer = JsonSerializer()
+        return serializer
